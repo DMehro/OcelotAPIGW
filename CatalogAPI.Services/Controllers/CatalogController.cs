e9aa64142023f59ad9046b6b5bc16a8080bbb434
Deependra Mehrotra <deependra.mehrotra@epsilon.com>
2020-08-09 13:11:54 -0500

    BaseSetup

---
+++ CatalogAPI.Services/Controllers/CatalogController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogAPI.Services.Controllers
+{
+    [Route("api/[controller]")]
+    public class CatalogController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Google Pixel", "Apple iPhone", "OnePlus Nord","Moto razor" };
+        }
+    }
+}
---
+++ CustomerAPI.Services/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerAPI.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Kylie J", "Justin B", "Cardy B", "Justin T" };
+        }
+    }
+}
